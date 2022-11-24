f031913805f9d3d22b15f4a61cad61922fed2572
Thaiebu <thaiebu785@gmail.com>
2022-11-24 16:46:33 +0530

    Code completed

--- room_app/admin.py
+++ room_app/admin.py
@@ -3,10 +3,10 @@ from .models import demo,Booking
 # Register your models here.
 
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'Name', 'gender')
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'Name', 'gender')
 
-admin.site.register(demo,BookAdmin)
+# admin.site.register(demo,BookAdmin)
 
 class BookRoom(admin.ModelAdmin):
     list_display = ('Room_No', 'Room_Type', 'Name','Name_2','Address','phone_number')
